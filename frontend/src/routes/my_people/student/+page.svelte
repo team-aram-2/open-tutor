39932074b1439f7e5856dc0d9e2acff05c3b0941
Caleb Smay <151682272+caleb-smay@users.noreply.github.com>
2024-11-24 20:57:19 -0600

    Basic My Tutors Interface (#9)
    Co-authored-by: Caleb S <calebsmay@my.unt.edu>
    Co-authored-by: Daniel Kaderli <drkaderli13@gmail.com>

---
+++ frontend/src/routes/my_people/student/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Tutor from '$lib/components/tutor-card.svelte';
+</script>
+
+<div>
+    <div style="display:flex; flex-direction: row; flex-wrap: wrap; gap: 75px;">
+
+        <Tutor name="Howdy Doo"></Tutor>
+        <Tutor name="Juniper Hovey"></Tutor>
+        <Tutor name="Nathan Jodoin"></Tutor>
+        <Tutor name="Jim Bob"></Tutor>
+    </div>
+</div>
