b463b9303cb163872186fec783f9bb813a6f9323
Juniper Hovey <me@juniper.link>
2024-12-08 23:25:33 -0600

    Finish API routes (#14)

--- backend/internal/services/impl.go
+++ backend/internal/services/impl.go
@@ -3,20 +3,42 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	"time"
+	"fmt"
 
 	openapi_types "github.com/oapi-codegen/runtime/types"
 )
 
-type OpenTutor struct {
-	Users map[string]User
-}
+type OpenTutor struct {}
 
 var _ ServerInterface = (*OpenTutor)(nil)
+var db *sql.DB
 
 func NewOpenTutor() *OpenTutor {
-	return &OpenTutor{
-		Users: make(map[string]User),
+	// Connect to database //
+	var conErr error
+	db, conErr = sql.Open("postgres", "postgres://postgres:developer@db:5432/postgres?sslmode=disable")
+	if conErr != nil {
+		log.Fatal(conErr)
 	}
+	// defer db.Close()
+
+	// Test connection //
+	conErr = db.Ping()
+	if conErr != nil {
+		log.Fatal(conErr)
+	}
+
+	// Set connection pool settings //
+	db.SetMaxOpenConns(30)
+	db.SetMaxIdleConns(30)
+  db.SetConnMaxLifetime(5 * time.Minute)
+	fmt.Printf("Connected to database\n")
+
+	return &OpenTutor{}
 }
 
 func sendError(w http.ResponseWriter, code int, message string) {
@@ -34,16 +56,35 @@ func (t *OpenTutor) CreateUser(w http.ResponseWriter, r *http.Request) {
 		sendError(w, http.StatusBadRequest, "Invalid format for user")
 		return
 	}
-	t.Users[string(user.UserId.String())] = user
+
+	var userId string
+	insertErr := db.QueryRow(`
+		INSERT INTO users (email, first_name, last_name)
+		VALUES ($1, $2, $3)
+		RETURNING user_id
+	`,
+		user.Email,
+		user.FirstName,
+		user.LastName,
+	).Scan(&userId)
+
+	if insertErr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "%s\n", insertErr)
+		return
+	}
+
 	w.WriteHeader(http.StatusCreated)
-	_ = json.NewEncoder(w).Encode(user)
+	json.NewEncoder(w).Encode(map[string]string{
+		"userId": userId,
+	})
 }
 
 func (t *OpenTutor) CreateMeeting(w http.ResponseWriter, r *http.Request) {
 	sendError(w, http.StatusMethodNotAllowed, "TODO")
 }
 
-func (t *OpenTutor) DeleteMeetingById(w http.ResponseWriter, r *http.Request, meetingId openapi_types.UUID) {
+func (t *OpenTutor) DeleteMeetingById(w http.ResponseWriter, r *http.Request, meetingId interface{}) {
 	sendError(w, http.StatusMethodNotAllowed, "TODO")
 }
 
@@ -95,6 +136,10 @@ func (t *OpenTutor) GetStudentByID(w http.ResponseWriter, r *http.Request, stude
 	sendError(w, http.StatusMethodNotAllowed, "TODO")
 }
 
+func (t *OpenTutor) GetRatingById(w http.ResponseWriter, r *http.Request, userId openapi_types.UUID, params GetRatingByIdParams) {
+	sendError(w, http.StatusMethodNotAllowed, "TODO")
+}
+
 func (t *OpenTutor) SignUpAsTutor(w http.ResponseWriter, r *http.Request) {
 	sendError(w, http.StatusMethodNotAllowed, "TODO")
 }
@@ -108,7 +153,28 @@ func (t *OpenTutor) DeleteUserById(w http.ResponseWriter, r *http.Request, userI
 }
 
 func (t *OpenTutor) GetUserById(w http.ResponseWriter, r *http.Request, userId openapi_types.UUID) {
-	sendError(w, http.StatusMethodNotAllowed, "TODO")
+	user := &User{}
+	selectErr := db.QueryRow(`
+		SELECT *
+		FROM users
+		WHERE user_id = $1
+	`, userId).Scan(
+		&user.UserId,
+		&user.Email,
+		&user.SignedUpAt,
+		&user.FirstName,
+		&user.LastName,
+		&user.AccountLocked,
+	)
+
+	if selectErr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "%s\n", selectErr)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(user)
 }
 
 func (t *OpenTutor) UpdateUserById(w http.ResponseWriter, r *http.Request, userId openapi_types.UUID) {
