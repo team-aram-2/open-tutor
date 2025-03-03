23759c4b5d579304b3f9b8e1bda553910f9e2e8b
Juniper Hovey <me@juniper.link>
2025-03-02 19:11:33 -0600

    Enable CORS

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func EnableCORS(next http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5174")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 		w.Header().Set("Access-Control-Allow-Methods", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
