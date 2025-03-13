6ab3a7b0a03801b7a144c6bcbc11638090b622c8
Caleb S <cal.t.sma@icloud.com>
2025-03-13 21:58:04 +0000

    Modified to maintain usability during resizing

--- frontend/src/lib/components/settings_page/font-size.svelte
+++ frontend/src/lib/components/settings_page/font-size.svelte
@@ -1,13 +1,75 @@
 <script lang="ts">
+	import { fontSize } from '$lib/stores';
+	import { get } from 'svelte/store';
+	let size = get(fontSize);
+
+	function setFontSize(value: string) {
+		console.log('Setting font size to ' + value);
+		fontSize.set(value);
+		console.log();
+	}
+
 	let value = 'normal';
 </script>
 
 <div class="font-size-buttons-container">
-	<input type="radio" name="font-size" value="very-small" class="font-size-button" />
-	<input type="radio" name="font-size" value="small" class="font-size-button" />
-	<input type="radio" name="font-size" value="normal" class="font-size-button" />
-	<input type="radio" name="font-size" value="large" class="font-size-button" />
-	<input type="radio" name="font-size" value="very-large" class="font-size-button" />
+	<p
+		class="text-size-indicator"
+		style="font-size: calc(max(calc(var(--font-size)/2), 20px); padding-right: 0px;"
+	>
+		A
+	</p>
+	<input
+		type="radio"
+		name="font-size"
+		value="very-small"
+		class="font-size-button rounded-l-2xl rounded-r-sm"
+		on:click={() => {
+			setFontSize('10px');
+		}}
+	/>
+	<input
+		type="radio"
+		name="font-size"
+		value="small"
+		class="font-size-button rounded-sm"
+		on:click={() => {
+			setFontSize('25px');
+		}}
+	/>
+	<input
+		type="radio"
+		name="font-size"
+		value="normal"
+		class="font-size-button rounded-sm"
+		on:click={() => {
+			setFontSize('45px');
+		}}
+	/>
+	<input
+		type="radio"
+		name="font-size"
+		value="large"
+		class="font-size-button rounded-sm"
+		on:click={() => {
+			setFontSize('60px');
+		}}
+	/>
+	<input
+		type="radio"
+		name="font-size"
+		value="very-large"
+		class="font-size-button rounded-r-2xl rounded-l-sm"
+		on:click={() => {
+			setFontSize('90px');
+		}}
+	/>
+	<p
+		class="text-size-indicator"
+		style="font-size: calc(max(var(--font-size), 45px); padding-left: 0px;"
+	>
+		A
+	</p>
 </div>
 
 <style>
@@ -33,12 +95,30 @@
 		height: calc(100% - 10px);
 		min-width: 25px;
 
-		border-radius: 15px;
 		background-color: var(--yellow-light);
 	}
 
 	.font-size-button:checked {
 		background-color: var(--yellow-neutral);
-		transition: 120ms background-color ease-in-out;
+		transition: 60ms background-color ease-in-out;
+	}
+
+	.text-size-indicator {
+		flex: 1;
+
+		height: calc(100% - 10px);
+
+		line-height: 100%;
+		align-content: center;
+		text-align: center;
+
+		font-weight: bolder;
+
+		color: var(--purple-dark);
+
+		padding: 0;
+		margin-top: 5px;
+		background-color: var(--purple-very-light);
+		border-radius: 15px;
 	}
 </style>
