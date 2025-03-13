a9c8a91cd5b452033cf5cc4704066d2d68660be7
Caleb S <cal.t.sma@icloud.com>
2025-03-13 21:59:04 +0000

    implemented persistent stores

--- frontend/src/lib/stores.ts
+++ frontend/src/lib/stores.ts
@@ -1,16 +1,25 @@
 import { writable, type Writable } from 'svelte/store';
 
-// Creates persistent svelte store through localStorage
+/**
+ * Creates persistent svelte store by leveraging localStorage.
+ * Does not create localStorage value until server-side rendering is complete.
+ */
 function createPersistentStore<T>(key: string, initialValue:T): Writable<T> {
-  // Attempt to retrieve previously stored value
-  const storedValue = localStorage.getItem(key);
+  // Attempt to retrieve previously stored value, check for server side rendering
+  let storedValue: T | null = null;
+  if(typeof window !== "undefined"){
+    const localStorageValue = localStorage.getItem(key);
+    storedValue = localStorageValue ? JSON.parse(localStorageValue) : null;
+  }
   // Create store
-  const store = writable(storedValue ? JSON.parse(storedValue) : initialValue)
+  const store = writable(storedValue !== null ? storedValue : initialValue)
 
   // Subscribe to store changes to update localStorage
-  store.subscribe((value) => {
-    localStorage.setItem(key, JSON.stringify(value));
-  });
+  if(typeof window !== "undefined"){
+    store.subscribe((value) => {
+      localStorage.setItem(key, JSON.stringify(value));
+    });
+  }
 
   return store;
 }
@@ -19,4 +28,5 @@ function createPersistentStore<T>(key: string, initialValue:T): Writable<T> {
 export const sessionToken = writable<string | null>(null);
 
 // Persistent stores
-export const fontSize = createPersistentStore<number>("fontSize", 16);
+export const fontSize = createPersistentStore<string>("fontSize", "16px");
+
