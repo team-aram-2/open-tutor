3e1ddd0884503b540e3e69d054d81bf68b5bec71
Alex Ho <hoalex219@gmail.com>
2025-03-15 16:12:49 -0500

    Prettier fixes

--- frontend/src/lib/stores.ts
+++ frontend/src/lib/stores.ts
@@ -1,6 +1,6 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store';
 
 const logged_in = writable(false);
-const user_id = writable("");
+const user_id = writable('');
 
-export {logged_in, user_id}
+export { logged_in, user_id };
