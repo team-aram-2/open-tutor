572eec37356a7e3c7976f936c13d0ea761ad89b6
Alex Ho <hoalex219@gmail.com>
2025-03-24 19:39:05 -0500

    prettier fixes

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,14 +1,12 @@
-import { redirect, type Handle } from "@sveltejs/kit";
+import { redirect, type Handle } from '@sveltejs/kit';
 
-export const handle: Handle = async ({event, resolve}) => {
-  console.log(event.url.pathname);
-  const protectedRoutes: string[] = ['/messages/student'];
-  if(protectedRoutes.includes(event.url.pathname)) {
-    const userCookie: string | undefined = event.cookies.get('session_token');
-    console.log(userCookie);
-    if (!userCookie) {
-      throw redirect(303, `/login?redirectTo=${event.url.pathname}`)
-    }
-  }
-  return resolve(event)
-}
+export const handle: Handle = async ({ event, resolve }) => {
+	const protectedRoutes: string[] = ['/messages/student'];
+	if (protectedRoutes.includes(event.url.pathname)) {
+		const userCookie: string | undefined = event.cookies.get('session_token');
+		if (!userCookie) {
+			throw redirect(303, `/login?redirectTo=${event.url.pathname}`);
+		}
+	}
+	return resolve(event);
+};
