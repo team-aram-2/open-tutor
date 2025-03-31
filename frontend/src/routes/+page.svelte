8cb7fc393cd592ae5de0a11091d58af12c522539
Alex Ho <hoalex219@gmail.com>
2025-03-30 23:30:30 -0500

    Make landing look better

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,7 +4,10 @@
 
 <main>
 	<div class="container">
-		<h1>OpenTutor</h1>
+		<div class="titlecontainer">
+			<h1 class="open">Open</h1>
+			<h1 class="tutor">Tutor</h1>
+		</div>
 		<div class="mission-statement">
 			<p>
 				Our mission is to make quality education accessible to everyone by connecting students with
@@ -21,25 +24,41 @@
 		justify-content: center;
 		align-items: center;
 		min-height: 100vh;
-		background-color: #231d34;
+		background-color: var(--purple-very-dark);
 	}
 
 	.container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
 		text-align: center;
 		max-width: 800px;
 		padding: 2rem;
 	}
 
 	h1 {
-		font-size: 3.5rem;
+		font-size: 6rem;
 		font-weight: 700;
-		color: #2563eb;
 		margin-bottom: 2rem;
+	}
+	.titlecontainer {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+	.open {
+		color: var(--yellow-very-light);
+	}
+	.tutor {
+		color: var(--yellow-neutral);
 	}
 
 	.mission-statement {
-		font-size: 1.25rem;
+		width: 75%;
+		font-size: 1rem;
 		line-height: 1.6;
-		color: var(--yellow-neutral);
+		color: var(--yellow-very-light);
+		text-align: center;
+		margin: 0px auto 0px auto;
 	}
 </style>
